92887160b4e43eba48dfda8a8592154f702cf8fb
Dima Ivanov <dima.ivanov.dev@gmail.com>
2025-04-07 17:16:20 +0200

    Initial commit.

---
+++ src/http.js
@@ -0,0 +1,13 @@
+export async function fetchAvailablePlaces() {
+  const response = await fetch('http://localhost:3000/places');
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error('Failed to fetch places.');
+  }
+
+  return resData.places;
+}
+
+export async function updateUserPlaces(places) {
+
+}
